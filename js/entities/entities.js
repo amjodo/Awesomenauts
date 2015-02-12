3676c41b85ed3cdb2e7015831c7fdaacc678d6a4
Amber Douglas <douglasamber3@gmail.com>
2015-02-12 10:57:12 -0800

    Finished video 22
    coded error with jumping

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,7 +10,9 @@ game.PlayerEntity = me.Entity.extend({
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
+		this.type = "PlayerEntity";
 		//sets velocity
+		this.health = 20;
 		this.body.setVelocity(5, 20);
 		//keeps track of where your character is going
 		this.facing ="right"; 
@@ -86,7 +88,12 @@ game.PlayerEntity = me.Entity.extend({
 		return true;
 	},
 
-		collideHandler:function(response){
+	loseHealth: function(damage){
+		this.health = this.health - damage;
+		console.log(this.health);
+	},
+	
+	collideHandler:function(response){
 			if(response.b.type==='EnemyBaseEntity'){
 				var ydif = this.pos.y - response.b.pos.y;
 				var xdif = this.pos.x -response.b.pos.x;
@@ -111,7 +118,7 @@ game.PlayerEntity = me.Entity.extend({
 					response.b.loseHealth();
 				}
 			}
-		}
+	}
 });
 //setting image heights and widths
 game.PlayerBaseEntity = me.Entity.extend({
@@ -239,6 +246,42 @@ game.EnemyCreep = me.Entity.extend({
 
 		return true;
 
+	},
+
+	collideHandler: function(response){
+		if(response.b.type==='PlayerBase'){
+			this.attacking=true;
+			this.body.vel.x = 0;
+			//keeps moving the creep tothe right to maintain its position
+			this.pos.x = this.pos.x + 1;
+			//checks that it has been at least 1 second since this creep
+			if((this.now-this.lastHit >= 1000)){
+				//updates the lastHit timer
+				this.lastHit = this.now;
+				//makes the player base call its loseHealth function
+				//damage of 1
+				response.b.loseHealth(1);
+			}
+		}else if (response.b.type ==='PlayerEntity'){
+			var xdif = this.pos.x - response.b.pos.x;
+
+			this.attacking=true;
+			//this.lastAttacking=this.now
+
+			if(xdif>0){
+				//keeps moving the creep tothe right to maintain its position
+				this.pos.x = this.pos.x + 1;
+				this.body.vel.x = 0;
+			}	
+			//checks that it has been at least 1 second since this creep
+			if((this.now-this.lastHit >= 1000) && xdif>0){
+				//updates the lastHit timer
+				this.lastHit = this.now;
+				//makes the player base call its loseHealth function
+				//damage of 1
+				response.b.loseHealth(1);
+			}
+		}
 	}
 });
 
