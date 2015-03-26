011060e207c507826ba1b2ddfccdc072523d9ae0
Amber Douglas <douglasamber3@gmail.com>
2015-03-25 21:38:33 -0700

    Finished video 47

--- js/screens/play.js
+++ js/screens/play.js
@@ -27,7 +27,7 @@ game.PlayScreen = me.ScreenObject.extend({
 		me.input.bindKey(me.input.KEY.B, "buy");
 		me.input.bindKey(me.input.KEY.Q, "skill1");
 		me.input.bindKey(me.input.KEY.W, "skill2");
-		me.input.bindKey(me.input.KEY.F, "skill3");
+		me.input.bindKey(me.input.KEY.E, "skill3");
 
 		//binding key
 		//moving player left or right or jump
