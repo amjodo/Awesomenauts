991a9724794fcc25963ab63d1fe71625d30d4038
Amber Douglas <douglasamber3@gmail.com>
2015-04-07 11:11:04 -0700

    Finished video 54
    still 2 errors from the last video

---
+++ php/controller/create-user.php
@@ -0,0 +1,40 @@
+
+<?php
+//goes into this file and completes instructions
+	require_once(__DIR__ . "/../model/config.php");
+//creating variables and storing these objects in the variable
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+//echos password & incripts it
+	$salt = "$5$5" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
+
+	$hashedPassword = crypt($password, $salt);
+
+	
+
+	$query = $_SESSION["connection"]->query("INSERT INTO users SET "
+		."email = '$email', "
+		."username = '$username',"
+		."password = '$hashedPassword',"
+		."salt = '$salt', "
+		."exp = 0, "
+		."exp1 = 0, "
+		."exp2 = 0, "
+		."exp3 = 0, "
+		."exp4 = 0, ");
+
+	$_SESSION("name") = $username;
+
+	if($query) {
+		//need for Ajax on index.php
+		echo "true";
+	}
+	else {
+		echo "<p>" . $_SESSION["connection"]->error . "</p>";
+	}
+?>
+<!--<link rel="stylesheet" type="text/css" href="main.css">
+<button type="button"><a href="blog.php">Home</a></button>-->
+
+
+
